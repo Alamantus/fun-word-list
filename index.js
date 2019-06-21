ed88b202be6ac29fc82bd0f28b87c4ea05989f60
Robbie Antenesse <dev@alamantus.com>
2019-06-21 13:41:16 -0600

    Add combined lists

---
+++ index.js
@@ -0,0 +1,11 @@
+module.exports = {
+  addons: require('./addons'),
+  adjectives: require('./adjectives'),
+  concepts: require('./concepts'),
+  descriptions: require('./descriptions'),
+  gametypes: require('./gametypes'),
+  locations: require('./locations'),
+  nouns: require('./nouns'),
+  superlatives: require('./superlatives'),
+  verbs: require('./verbs'),
+};
---
+++ verbs/index.js
@@ -0,0 +1,5 @@
+module.exports = {
+  secondPerson: require('./secondPerson'),
+  secondConcepts: require('./secondConcepts'),
+  thirdPerson: require('./thirdPerson'),
+};
